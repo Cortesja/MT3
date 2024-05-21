f2bfd0035e66123884ce887e0c167effe40e8675
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-05-21 13:11:58 +0900

    完成
    プッシュ

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,6 +1,29 @@
 #include <Novice.h>
+#include "Matrix4x4.h"
+#include "Vector3.h"
 
-const char kWindowTitle[] = "GC2B_05_コーテスジャレッドアレン";
+const char kWindowTitle[] = "GC1B_05_コーテスジャレッドアレン";
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
+}
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* str)
+{
+	Novice::ScreenPrintf(x, y + kRowHeight, "%s", str);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				(x + column * kColumnWidth) + kRowHeight, (y + row * kRowHeight) + kRowHeight * 2, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -9,8 +32,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -25,9 +48,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 translate{ 4.1f, 2.6f, 0.8f };
+		Vector3 scale{ 1.5f, 5.2f, 7.3f };
+		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Vector3 point{ 2.3f, 3.8f, 1.4f };
+		Matrix4x4 transformMatrix = {
+			1.0f, 2.0f, 3.0f, 4.0f,
+			3.0f, 1.0f, 1.0f, 2.0f,
+			1.0f, 4.0f, 2.0f, 3.0f,
+			2.0f, 2.0f, 1.0f, 3.0f
+		};
+		Vector3 transformed = Transform(point, transformMatrix);
+
 		///
 		/// ↑更新処理ここまで
 		///
+
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, 0, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, scaleMatrix, "scaleMatrix");
 
 		///
 		/// ↓描画処理ここから
