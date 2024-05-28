cdcdeab0e8dfa1eb894aec808635c725ee73c50b
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-05-28 14:23:01 +0900

    更新

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,29 +1,12 @@
 #include <Novice.h>
-#include "Matrix4x4.h"
-#include "Vector3.h"
+#include "Matrix4x4f.h"
+#include "Vector3f.h"
+using MyNamespace::Matrix4x4f;
 
 const char kWindowTitle[] = "GC1B_05_コーテスジャレッドアレン";
 
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
-
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
-	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
-	Novice::ScreenPrintf(x + kColumnWidth, y, "%.02f", vector.y);
-	Novice::ScreenPrintf(x + kColumnWidth * 2, y, "%.02f", vector.z);
-	Novice::ScreenPrintf(x + kColumnWidth * 3, y, "%s", label);
-}
-
-void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* str)
-{
-	Novice::ScreenPrintf(x, y + kRowHeight, "%s", str);
-	for (int row = 0; row < 4; ++row) {
-		for (int column = 0; column < 4; ++column) {
-			Novice::ScreenPrintf(
-				(x + column * kColumnWidth) + kRowHeight, (y + row * kRowHeight) + kRowHeight * 2, "%6.02f", matrix.m[row][column]);
-		}
-	}
-}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -34,6 +17,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	Vector3f temp = {};
+	Matrix4x4f tempMatrix = {};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -50,8 +36,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		Vector3 translate{ 4.1f, 2.6f, 0.8f };
 		Vector3 scale{ 1.5f, 5.2f, 7.3f };
-		Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-		Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+		Matrix4x4 translateMatrix = tempMatrix.MakeTranslateMatrix(translate);
+		Matrix4x4 scaleMatrix = tempMatrix.MakeScaleMatrix(scale);
 		Vector3 point{ 2.3f, 3.8f, 1.4f };
 		Matrix4x4 transformMatrix = {
 			1.0f, 2.0f, 3.0f, 4.0f,
@@ -59,15 +45,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			1.0f, 4.0f, 2.0f, 3.0f,
 			2.0f, 2.0f, 1.0f, 3.0f
 		};
-		Vector3 transformed = Transform(point, transformMatrix);
+		Vector3 transformed = tempMatrix.Transform(point, transformMatrix);
 
 		///
 		/// ↑更新処理ここまで
 		///
 
-		VectorScreenPrintf(0, 0, transformed, "transformed");
-		MatrixScreenPrintf(0, 0, translateMatrix, "translateMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5, scaleMatrix, "scaleMatrix");
+		temp.VectorScreenPrintf(0, 0, transformed, "transformed");
+		tempMatrix.MatrixScreenPrintf(0, 0, translateMatrix, "translateMatrix");
+		tempMatrix.MatrixScreenPrintf(0, kRowHeight * 5, scaleMatrix, "scaleMatrix");
 
 		///
 		/// ↓描画処理ここから
